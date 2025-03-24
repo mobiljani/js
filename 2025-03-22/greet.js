eb2db97fef3dfad67eb4246c24214fe5a73418a2
goodboi24 <roborexraptor@gmail.com>
2025-03-24 20:23:49 +0000

    implemented reverse

---
+++ 2025-03-22/greet.js
@@ -0,0 +1,8 @@
+
+
+function greet (name) {
+console.log (`hello ${name}`)
+}
+
+
+greet(`zachary`)
---
+++ 2025-03-22/reverse.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+function reverse(number) {
+    let rstring = number.toString()
+    
+    for (let index = rstring.length; index > 0; index = index-1) {
+        console.log (rstring[index-1])
+    }
+
+    // for loop and print
+}
+
+reverse(635502123)
